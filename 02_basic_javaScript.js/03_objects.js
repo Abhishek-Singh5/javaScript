8a1257bf86233f40280cd2e137679da903a84b37
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-07-01 00:54:42 +0530

    Add files via upload

---
+++ 02_basic_javaScript.js/03_objects.js
@@ -0,0 +1,30 @@
+const course = {
+    coursename: "JavaScript",
+    price: "1999",
+    courseInstructor: "hitest"
+}
+
+// console.log(course.courseInstructor);
+
+// const {courseInstructor} = course;
+// console.log(courseInstructor);
+
+const {courseInstructor: instructor} = course;
+// console.log(instructor);
+
+
+// json formate key and value both are string
+
+// {
+//     "name": "hitesh",
+//     "coursename": "JavaScript",
+//     "price": "free"
+// }
+
+// Array in JSON formate
+
+[
+    {},
+    {},
+    {}
+]
